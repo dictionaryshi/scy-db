08505134dc0221c4715c6c2a4ce3e5091ea4a3c3
史春阳 <shichunyang02@meituan.com>
2020-09-25 17:12:48 +0800

    DbConstant.java

---
+++ src/main/java/com/scy/db/constant/DbConstant.java
@@ -0,0 +1,27 @@
+package com.scy.db.constant;
+
+/**
+ * DbConstant
+ *
+ * @author shichunyang
+ * Created by shichunyang on 2020/9/25.
+ */
+public class DbConstant {
+
+    private DbConstant() {
+    }
+
+    public static final String SELECT_LAST_INSERT_ID = "SELECT LAST_INSERT_ID()";
+
+    public static final String DATA_SOURCE = "DataSource";
+
+    public static final String DATA_SOURCE_MASTER = "DataSourceMaster";
+
+    public static final String DATA_SOURCE_SLAVE = "DataSourceSlave";
+
+    public static final String TRANSACTION_MANAGER = "TransactionManager";
+
+    public static final String SQL_SESSION_FACTORY = "SqlSessionFactory";
+
+    public static final String SQL_SESSION_TEMPLATE = "SqlSessionTemplate";
+}
