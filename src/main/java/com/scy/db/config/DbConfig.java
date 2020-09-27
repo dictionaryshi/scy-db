71e59b6faacf0233a6e5ce010216d1ac09bcd852
史春阳 <shichunyang02@meituan.com>
2020-09-27 11:06:30 +0800

    DbConfig.java

--- src/main/java/com/scy/db/config/DbConfig.java
+++ src/main/java/com/scy/db/config/DbConfig.java
@@ -1,10 +1,13 @@
 package com.scy.db.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+
 /**
  * DbConfig
  *
  * @author shichunyang
  * Created by shichunyang on 2020/9/27.
  */
+@ConditionalOnProperty(value = "dbConfig.enabled", havingValue = "true", matchIfMissing = true)
 public class DbConfig {
 }
